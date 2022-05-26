8ff5c86d06ce127db87cb4e151b8b1030f074f44
sergio murilo hardt marques <sergiohardt94@gmail.com>
2022-05-26 02:00:25 -0300

    initial

--- src/main/java/br/com/application/cleanarcexample/CleanarcExampleApplication.java
+++ src/main/java/br/com/application/cleanarcexample/CleanarcExampleApplication.java
@@ -1,13 +1,13 @@
-package com.demo;
+package br.com.application.cleanarcexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class CleanarcExampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(CleanarcExampleApplication.class, args);
 	}
 
 }
